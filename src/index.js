48284d21c887fb56fc478b489a3e893365517889
sylvaincodes <sylvaincodeur@gmail.com>
2023-07-28 11:49:10 +0200

    initalation de node sass , fichiers assets , bootstrap 4.5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/scss/style.scss'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/scss/style.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
