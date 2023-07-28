b1c5fc5057a029fbdcc26f01042ac8a20d99c190
sylvaincodes <sylvaincodeur@gmail.com>
2023-07-28 14:02:22 +0200

    Structuration App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import React, { Suspense,lazy }  from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+
+
+const Home1 = lazy( () => import("./pages/home/Home1.jsx"));
+
+const App = () => {
   return (
-    <div>
-      <a name="" id="" class="btn btn-primary" href="#" role="button">Button</a>
-    </div>
-  );
+    <Suspense fallback={ <div>Chargement.....</div> }>
+      <BrowserRouter>
+          <Routes>
+              <Route exact path='/' Component={Home1}/>
+          </Routes>      
+      </BrowserRouter>
+    </Suspense>
+  )
 }
 
-export default App;
+export default App
